71ce69a5feaeecb3751599536a4735d0698e9298
Braeden Lyman <braedenlyman@Braedens-MacBook-Air.local>
2025-04-21 23:07:25 -0400

    added headings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+import '@radix-ui/themes/styles.css';
+import { Heading, Theme, Flex } from '@radix-ui/themes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>My Portfolio</h2>
-      </header>
-    </div>
+    <Theme appearance="dark" accentColor="indigo" panelBackground="solid">
+      <Flex direction="column" gap="5">
+        <Heading size="9" className="heading">Hi. I'm Braeden.</Heading>
+        <Heading size="9" className="heading2">A Developer.</Heading>
+      </Flex>
+    </Theme>
   );
 }
-
 export default App;
