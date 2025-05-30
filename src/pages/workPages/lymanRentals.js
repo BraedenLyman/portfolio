efab632c160ad9efaa1e13818e216466ac46199e
BraedenLymanQK <108896258+BraedenLyman@users.noreply.github.com>
2025-05-30 09:10:10 -0400

    updated lymanRentals page and deployed

--- src/pages/workPages/lymanRentals.js
+++ src/pages/workPages/lymanRentals.js
@@ -8,7 +8,10 @@ function LymanRentals() {
       title="Lyman Rentals"
       category="Web Development"
       year="2025"
-      description="A revolutionary quote generation platform with AI integration."
+      description="Inspired by my brother’s wedding, where we handmade decor and event items, my family and I launched Lyman Rentals. It is a wedding rental business offering unique, handcrafted pieces. To establish our online presence quickly, I built our website lymanrentals.ca using Wix, prioritizing speed and ease of use.
+      The site features a clean, visually appealing design with high-quality images of our rentals, easy navigation for browsing inventory, and a seamless contact system for inquiries and bookings. By using Wix, I efficiently created a professional, mobile-friendly website without extensive coding, allowing us to start promoting our business right away.
+
+This project combined creativity with practical execution, turning a personal experience into a functional and marketable online business."
       prevProject={{ name: "ViB Digital", url: "/work/vib-digital" }}
       nextProject={{ name: "Windrush Technical", url: "/work/windrush-tech" }}
     >
